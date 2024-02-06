e02a5ad100fd66fdec1d9d29758c99a5f85da593
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-02-06 16:25:15 +0100

    doctor new, create and index is working

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,27 @@
 class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    @doctor.user = current_user
+    @doctor.save
+
+    if @doctor.save
+      redirect_to doctors_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:first_name, :last_name, :specialty, :email)
+  end
 end
