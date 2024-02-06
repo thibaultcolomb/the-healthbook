d9ea26b0bcfb6226aa0a84a29e91aa5dbffb44a3
Kemi Oyinloye <drkemi01@gmail.com>
2024-02-06 11:56:06 +0000

    added the new appointments page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,25 @@
 class AppointmentsController < ApplicationController
+  def new
+    @appointment = Appointment.new
+    @doctors = Doctor.all
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+
+    if @appointment.save
+      redirect_to @appointment, notice: 'Appointment was successfully created.'
+    else
+      @doctors = Doctor.all
+      render :new
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:date, :report_image, :doctor_id)
+  end
+
+
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
-  belongs_to :user, through: :doctors
+  has_many :users
   has_many :reports, through: :appointment_attachements
 
   validates :date, :content, presence: true
