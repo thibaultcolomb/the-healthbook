136748d943745ced11c13556a292288cbb17698d
Thibault Colomb <thibault.colomb934@gmail.com>
2024-02-06 12:04:18 +0100

    created index,new and create methods in reports controller, added index html to visualise

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,22 @@
 class ReportsController < ApplicationController
+
+  def index
+    @reports = Report.all
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    @report.save
+
+    if @report.save
+      redirect_to reports_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
