39c5470d13a0f07d65c349fbabdeda69037ddeb4
Kemi Oyinloye <drkemi01@gmail.com>
2024-02-06 14:58:26 +0000

    appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,21 +8,44 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.new(appointment_params)
 
     if @appointment.save
-      redirect_to @appointment, notice: 'Appointment was successfully created.'
+      redirect_to appointment_path
     else
-      @doctors = Doctor.all
-      render :new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    if @appointment.update(appointment_params)
+      redirect_to appointment_path(@appointment)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def index
     @appointments = Appointment.all
   end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:appointment_date, :content, :doctor_id)
+  end
+
 end
+
 
 
   private
 
   def appointment_params
-    params.require(:appointment).permit(:date, :report_image, :doctor_id)
+    params.require(:appointment).permit(:appointment_date, :content, :doctor_id)
+  end
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
   end
