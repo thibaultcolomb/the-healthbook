3d407b362de176ec466adcc805b271c05e51082b
Thibault Colomb <thibault.colomb934@gmail.com>
2024-02-06 15:29:07 +0100

    added read a picture function

--- app/models/report.rb
+++ app/models/report.rb
@@ -11,4 +11,6 @@ class Report < ApplicationRecord
 
   include PgSearch::Model
   pg_search_scope :search_by_title, against: [:title], using: { tsearch: { prefix: true } }
+
+  has_one_attached :photo
 end
