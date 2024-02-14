058e11460958a638fc97d0da1d1810e166385154
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-02-14 10:15:06 +0100

    second slide is smaller and navigation is working fine

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -3,7 +3,6 @@ import Swiper from "swiper"
 // Connects to data-controller="swiper"
 export default class extends Controller {
   connect() {
-    console.log("hi")
     this.swiper = new Swiper('.swiper-container', {
       spaceBetween: 30,
       freeMode: true,
