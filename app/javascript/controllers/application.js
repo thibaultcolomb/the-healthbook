ee684c24dfde2eb4174ffa33aa5b87144a24e442
Thibault Colomb <thibault.colomb934@gmail.com>
2024-02-14 14:19:57 +0100

    js change

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -11,5 +11,5 @@ window.Stimulus   = application
 // Stimulus.register("show-doctors", ShowDoctorsController)
 
 export { application }
-import ReportTypeController from "../controllers/report_type_controller"
+import ReportTypeController from "../controllers/report_type_controller.js"
 Stimulus.register("report-type", ReportTypeController)
