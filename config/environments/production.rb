15362360099f8fcca573ffdaf41b05f00a624cd6
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-02-15 10:23:17 +0100

    no changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://the-healthbook-59003676b783.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "the-healthbook-59003676b783.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
